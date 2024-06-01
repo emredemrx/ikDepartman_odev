5a6a4479397c3ed8b6b59b5cbeb8200b4ecd7fa7
emred <femredemir699@gmail.com>
2024-06-02 00:09:22 +0300

    ödev 5

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,2 +1,37 @@
-package org.example;public class Person {
+package org.example;
+
+public class Person {
+
+    String name;
+    String email;
+    double wage;
+    int workingDay;
+
+    int defaultday = 25;//bir ayda varsayılan çalışma günü
+    double overtimeWage = 1000.00; // gunluk mesai ücreti
+
+
+    void maasHesapla(){
+
+        String hakMesaji  =  ( workingDay > 31 && workingDay < 1  ) ? "Personel maaş almaya hak kazanmış Personel maaşı: " : "Girilen günler maaş için geçerli değildir. Maaslar aylık gun sayısına göre hesaplanmaktadır.";
+
+        if (workingDay == defaultday ){
+            System.out.println(name +" isimli personelin bu ay maası: "+wage);
+        }else if (workingDay < defaultday) {
+            double gunlukMaas = wage / defaultday;
+            System.out.println(name +" isimli personelin bu ay maası: "+gunlukMaas*workingDay);
+        }else{
+            if (workingDay >= 1 && workingDay <= 31) {
+                primMaas();
+            }else {
+                System.out.println(hakMesaji);
+            }
+        }
+    }
+
+    void primMaas () {
+        int calisilanEkGunSayisi = workingDay - defaultday;
+        double mesaiUcreti = overtimeWage* calisilanEkGunSayisi;
+        System.out.println(name +" isimli personelin bu ay maası: "+ mesaiUcreti+wage);
+    }
 }
